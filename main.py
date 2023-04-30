54fe303687c596c79a11829f67d82ccbed87e441
Desmos Enjoyer <128858049+MX-boop@users.noreply.github.com>
2023-04-30 17:02:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ print("Game Options")
 print()
 print("1--QuantumTool")
 print("2--Mad Libs")
+print("3--RandomFact")
 print()
 userinput = int(input("What Game Would You Like To Play(Number)"))
 if userinput == 1:
     call(["python", "Games/QuantumTool.py"])
 elif userinput == 2:
+    call(["python", "Games/MadLibs.py"])
+elif userinput == 3:
     call(["python", "Games/MadLibs.py"])
