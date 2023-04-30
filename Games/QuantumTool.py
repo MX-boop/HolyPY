988260ec3ae64c5af1f9475f5a25c4baa5f57f5a
Desmos Enjoyer <128858049+MX-boop@users.noreply.github.com>
2023-04-30 16:46:27 -0400

    Add files via upload

---
+++ Games/QuantumTool.py
@@ -0,0 +1,78 @@
+﻿import uuid
+import time
+import random
+import sys
+
+def printProgressBar(iteration, total, prefix="", suffix="", decimals=1, length=100, fill="█", printEnd="\r"):
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + "-" * (length - filledLength)
+    sys.stdout.write("\r%s |%s| %s%% %s" % (prefix, bar, percent, suffix))
+    if iteration == total:
+        sys.stdout.write("\n")
+    sys.stdout.flush()
+
+
+def Print(number: int):
+    colors = [
+        "\033[37m"   # white
+    ]
+    
+    messages = [
+        "Successfully cloned into ",
+        "Error: Failed to clone into ",
+        "Information: The following files have been cloned into ",
+        "Clone completed successfully into ",
+        "Clone failed. Please check the URL: ",
+        "Cloning in progress: ",
+        "Clone successful. Files copied into ",
+        "Warning: Clone may have failed. Check the URL: ",
+        "Clone completed with errors. See output below: "
+    ]
+    
+    urls = [
+        "https://aptpackages.ubuntu.org/",
+        "https://www.google.com/",
+        "https://github.com/",
+        "https://stackoverflow.com/",
+        "https://docs.python.org/",
+        "https://www.amazon.com/",
+        "https://www.reddit.com/",
+        "https://www.microsoft.com/",
+        "https://www.apple.com/",
+        "https://www.spotify.com/"
+    ]
+    
+    for i in range(1, number+1):
+        message = random.choices(messages, weights=[10, 2, 8, 10, 3, 7, 10, 1, 3], k=1)[0]
+        url = random.choice(urls)
+        teal_color_code = random.choice(colors)
+        url_color_code = random.choice(colors)
+        deep_teal_color_code = random.choice(colors)
+        reset_color_code = "\033[0m"
+        uuid_str = str(uuid.uuid4())
+        
+        if "Error" in message:
+            message_color_code = "\033[31m"
+        else:
+            message_color_code = teal_color_code
+            
+        print(f"{message_color_code}{message}{url_color_code}{url}{reset_color_code}{deep_teal_color_code}{uuid_str}{reset_color_code}")
+        
+        # progress bar with varying speeds occasionally
+        if random.random() < 0.3:
+            for j in range(101):
+                speed = random.uniform(0.001, 0.001)  # speed
+                time.sleep(speed)
+                printProgressBar(j, 100, prefix=f"Progress [{i}/{number}] ", suffix="Complete", length=50)
+        
+        # delay for the next message
+        time.sleep(random.uniform(0.002, 0.0008))
+        
+        # terminal animation
+        if random.random() < 0.3:
+            print(f"{teal_color_code}Working...{reset_color_code}", end="", flush=True)
+            time.sleep(0.3)
+            print("\b")
+
+Print(number=500)
