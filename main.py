de7de97b16a1e425b1e800ff9fbfd3426b6d014c
Desmos Enjoyer <128858049+MX-boop@users.noreply.github.com>
2023-05-01 14:29:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,70 @@
 from subprocess import call
 
+import time
+import sys
+import os
+  
+def load_animation():
+  
+    load_str = "starting your console application..."
+    ls_len = len(load_str)
+ 
+    animation = "|/-\\"
+    anicount = 0
+      
+    counttime = 0        
+      
+    # pointer for travelling the loading string
+    i = 0                     
+  
+    while (counttime != 100):
+          
+        time.sleep(0.075) 
+                              
+        
+        load_str_list = list(load_str) 
+          
+        x = ord(load_str_list[i])
+          
+        # y->for storing altered ASCII code
+        y = 0                             
+  
+        if x != 32 and x != 46:             
+            if x>90:
+                y = x-32
+            else:
+                y = x + 32
+            load_str_list[i]= chr(y)
+          
+        # for storing the resultant string
+        res =''             
+        for j in range(ls_len):
+            res = res + load_str_list[j]
+              
+        # displaying the resultant string
+        sys.stdout.write("\r"+res + animation[anicount])
+        sys.stdout.flush()
+  
+        # Assigning loading string
+        # to the resultant string
+        load_str = res
+  
+          
+        anicount = (anicount + 1)% 4
+        i =(i + 1)% ls_len
+        counttime = counttime + 1
+      
+    # for windows OS
+    if os.name =="nt":
+        os.system("cls")
+          
+    # for linux / Mac OS
+    else:
+        os.system("clear")
+  
+if __name__ == '__main__': 
+    load_animation()
+
 print('Welcome To A Ton Of Games')
 print("Game Options")
 print()
