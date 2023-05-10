422335d53296b52d04310236e9048d253fdb37b8
Desmos Enjoyer <128858049+MX-boop@users.noreply.github.com>
2023-05-10 11:54:11 -0400

    Add French

---
+++ Games/FrenchGames/IRverbs.py
@@ -0,0 +1,22 @@
+import random
+import string
+
+print("Welcome To French IR verb Conjugations")
+print()
+print("""
+-IR Verbs are normally used in the context of becoming/being something
+ex: rougir means to become red/to blush
+ex: grandir means to grow
+""")
+
+print(f"""
+Most -IR verbs have a standerd rule for conjugations:
+Verb: finir
+Je Form   | Je Fin\033[036mis\033[0m
+Tu Form   | Tu Fin\033[036mis\033[0m
+Il Form   | Il Fin\033[036mit\033[0m
+Nous Form | Nous Fin\033[036missons\033[0m
+Vous Form | Vous Fin\033[036missez\033[0m
+Ils Form  | Ils fin\033[036missent\033[0m
+""")
+print("I Hope This Works")
--- Games/IRFrench.py
+++ Games/IRFrench.py
@@ -1,1 +1,12 @@
+from subprocess import call
 
+print("Welcome To The French Classroom")
+print("What Would You Like To Learn About")
+
+print("1--IR verb conjugations")
+
+usr_input = input("What Is You Selection")
+
+if usr_input == 1:
+    print("Sounds Good!")
+    call(["python3", "FrenchGames/IRverbs.py"])
