fe7f301d42a4431e73a1ba67e81962ed3c04e8bc
Desmos Enjoyer <128858049+MX-boop@users.noreply.github.com>
2023-05-11 11:21:51 -0400

    finished?

--- Games/History.py
+++ Games/History.py
@@ -93,7 +93,7 @@ def Message1():
     tricity in a house-hold setting. The event featured many interiors equiped with edis
     ion bulbs to illuminate the fair. One of the major expositions included a AC inducti
     on motor invented by Nikola Tesla. The World's Fair took place at the tail end of th
-    e War of currants, a battle between AC (Alternating Current) and DC (Direct Current)
+    e War of currents, a battle between AC (Alternating Current) and DC (Direct Current)
     """)
 
     print()
@@ -101,6 +101,40 @@ def Message1():
     <--Nikola Tesla                                                   War Of Currents-->""")
 
 Pointer = True
+
+def MessageSel():
+    if Pointer:
+        clear()
+        print("""
+        Nikola Tesla was a Serbian-American inventer who is most well known for his con
+        tribution to eletrical engineering in the late 1800's. Many of his inventions w
+        ere featured at the World's Fair in Chicago. One of Tesla's most well know inve
+        ntions is the Tesla Coil. The coil was used for creating high voltage but low c
+        urrent electricity. The coil was essential in the development of wireless trans
+        mitters. Technology that was pioneered by the Tesla coil is still widely used i
+        n many long-range radio transmiters today.
+        """)
+        PrintSlope(slope = 2)
+        time.sleep(10)
+
+    else:
+        clear()
+        print("""
+        The War of Currents was a battle between AC and DC that occurred in the mid to l
+        ate 1800's. The two main combatants in the fight were Thomas Edison(DC) and Geor
+        ge Westinghouse. Thomas was a american inventer who was the founder of the Ediso
+        n Illuminating Company. The company was aimed at competing with the market domma
+        nt gas lamps by creating less expensive DC lamps. The Westinghouse Electric Comp
+        any was founded by George Westinghouse in 1884 and focused on using AC technolog
+        ies to provide eletricity to a large amount of households and businesses. the tw
+        o companys fought for space in the new eletricity market. Before the 1893 fair t
+        here was a feirce compition to power the lights for the events which was eventua
+        ly won by AC. After Edison left his company the world started to see the winding
+        down of the War of Currents with AC as its victor. We can still see the products
+        of the battle today as AC is used to almost every type of eletric application.
+        """)
+        PrintSlope(slope = 0.5)
+        time.sleep(10)
 
 while Running == True:
     clear()
@@ -117,46 +151,15 @@ while Running == True:
     
     print()
 
-    user_input = input("Enter 'E' to end the loop: ")
+    user_input = input("Enter C To View Citations, Enter 'E' to Select: ")
 
     if user_input == "E": #If The User Enters E The loop ends -> moves to selected option
-        break
+        MessageSel()
+    elif user_input == "C":
+        print("""
+        Citations""")
     else:
         if Pointer: # Else Toggles Pointer
             Pointer = False
         else:
             Pointer = True
-
-clear()
-
-if Pointer:
-    print("""
-    #thing abt nic tesla
-    """)
-    PrintSlope(slope = 2)
-else:
-    print("""
-    #thing abt war of currents
-    """)
-    PrintSlope(slope = 0.5)
-
-UsrXcrd = 5
-UsrYcrd = 5
-
-while Running:
-
-    clear()
-
-    PrintXY(Xcrd = UsrXcrd, Ycrd = UsrYcrd)
-    print()
-    print("WSAD")
-    UsrControl = input("Control: ")
-
-    if UsrControl == "w": #Changes Usr Cords For WSAD input
-        UsrYcrd += -1
-    elif UsrControl == "s":
-        UsrYcrd += 1
-    elif UsrControl == "d":
-        UsrXcrd += 1
-    elif UsrControl == "a":
-        UsrXcrd += -1
